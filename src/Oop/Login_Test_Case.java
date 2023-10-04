6aa3ec785379a30dc0d3983001b70a594cb16207
Noor <noor.non1993@gmail.com>
2023-10-04 22:37:05 +0300

    n

--- src/Oop/Login_Test_Case.java
+++ src/Oop/Login_Test_Case.java
@@ -1,13 +1,15 @@
 package Oop;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
 public class Login_Test_Case  extends Parameters  {
-	
+	Parameters  myMagento= new Parameters();
 	WebDriver driver= new ChromeDriver();
 	
 	@BeforeTest
@@ -20,6 +22,21 @@ public class Login_Test_Case  extends Parameters  {
 	
 	@Test()
 	public void theTest() {
+		
+		WebElement signIn=driver.findElement(By.xpath("//div[@class='panel header']//a[contains(text(),'Sign In')]"));
+		signIn.click();	
+		
+		WebElement logIn=driver.findElement(By.id("email"));
+		logIn.sendKeys(myMagento.logInEmail);
+		
+		WebElement pAssword=driver.findElement(By.id("pass"));
+		pAssword.sendKeys(myMagento.confiRm);
+		
+		
+		WebElement eNter=driver.findElement(By.id("send2"));
+		eNter.click();		
+		
+		
 		
 		
 	}
--- src/Oop/Parameters.java
+++ src/Oop/Parameters.java
@@ -1,55 +1,36 @@
 package Oop;
+
 import java.security.SecureRandom;
 import java.util.Random;
 
-
 public class Parameters {
 	static String myURL = "https://magento.softwaretestingboard.com";
-	static String[] Arrynames = { "noor", "roaa", "heba", "huda", "dana", "hanaa", "aya" };
-    static String[] Arrylastnames = { "Ahmad", "yousef", "amr",  "moath", "mohammad", "obada" };
-    static Random random = new Random();
-   static  int randomFirstIndex = random.nextInt(Arrynames.length);
-    static int randomLastIndex = random.nextInt(Arrylastnames.length);
-        static String randomFirstName = Arrynames[randomFirstIndex];
-        static String randomLastName = Arrylastnames[randomLastIndex];
-        
-        static String myMail = randomFirstName +randomLastName +"@gmail.com" ;
-        static String passWord=generatePassword(8);
-        static String confiRm= passWord ;
-   
-   private static final String CHARACTERS = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()_-+=<>?";
-
-        		
-       public static String generatePassword(int length) {
-          StringBuilder password = new StringBuilder();
-            SecureRandom random = new SecureRandom();
-
-            for (int i = 0; i < length; i++) {
-                int randomIndex = random.nextInt(CHARACTERS.length());
-                char randomChar = CHARACTERS.charAt(randomIndex);
-                password.append(randomChar);
-            }
-
-            return password.toString();
-        }
-    }
-         
-        
-    
-        
-        
-	
+	static String[] Arrynames = { "noor", "roaa", "heba", "huda", "dana", "hanaa", "aya" ,"summaya","alaa"};
+	static String[] Arrylastnames = { "Ahmad", "yousef", "amr", "moath", "mohammad", "obada" ,"mahmoud"};
+	static Random random = new Random();
+	static int randomFirstIndex = random.nextInt(Arrynames.length);
+	static int randomLastIndex = random.nextInt(Arrylastnames.length);
+	static String randomFirstName = Arrynames[randomFirstIndex];
+	static String randomLastName = Arrylastnames[randomLastIndex];
 	
 
- 
+	static String myMail = randomFirstName + randomLastName + "@gmail.com";
+	static String logInEmail=myMail;
+	static String passWord = generatePassword(8);
+	static String confiRm = passWord;
 
- 
+	private static final String CHARACTERS = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()_-+=<>?";
 
+	public static String generatePassword(int length) {
+		StringBuilder password = new StringBuilder();
+		SecureRandom random = new SecureRandom();
 
+		for (int i = 0; i < length; i++) {
+			int randomIndex = random.nextInt(CHARACTERS.length());
+			char randomChar = CHARACTERS.charAt(randomIndex);
+			password.append(randomChar);
+		}
 
-
- 
-
-
-
-   
+		return password.toString();
+	}
+}
